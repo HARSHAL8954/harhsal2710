aa6a255752eca7a1455e25044d8e82b6b2f1971c
HARSHAL8954 <harshal2525@gmail.com>
2025-03-22 23:14:12 +0530

    Library Management System

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from './shared/shared.module';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, SharedModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'UI';
+}
